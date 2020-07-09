d8a425fd633d1487faf40669110081210dd84313
AustinPotts <pottsevolvements@gmail.com>
2020-07-08 22:25:23 -0400

    Configured AR TV + SnowBalls

--- iSmart/iSmart/ViewController.swift
+++ iSmart/iSmart/ViewController.swift
@@ -20,7 +20,34 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-     
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/tv_retro.usdz")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let myTouch = touches.first else { return }
+        let myResult = sceneView.hitTest(myTouch.location(in: sceneView), types: [ARHitTestResult.ResultType.featurePoint])
+       guard let hitResult = myResult.last else {return}
+        
+        let hitTransform = SCNMatrix4.init(hitResult.worldTransform)
+        
+        let hitVector = SCNVector3Make(hitTransform.m41, hitTransform.m42, hitTransform.m43)
+        
+        createBall(position: hitVector)
+    }
+    
+    func createBall(position: SCNVector3) {
+        var ballShape = SCNSphere(radius: 0.01)
+        var ballNode = SCNNode(geometry: ballShape)
+        ballNode.position = position
+        sceneView.scene.rootNode.addChildNode(ballNode)
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -40,5 +67,29 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
 }
